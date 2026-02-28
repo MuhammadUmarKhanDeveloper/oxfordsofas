5617be443c3a2f7160808be9cae00b7dcf7e85e4
MuhammadUmarKhanDeveloper <umerfromaptech@gmail.com>
2026-02-28 12:07:14 +0500

    FIrst Commit

---
+++ Final Project 18-02-2026/Final Project/navbar.js
@@ -0,0 +1,50 @@
+var nav=document.getElementById("navbar")
+.innerHTML=`
+
+<nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+          <img src="Assist/image/logo 01.png" alt="" width="110px" height="110px">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 gap-5">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="home.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="About .html">About</a>
+              </li>
+
+<li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Brands 
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="Classic .html">Classic</a></li>
+            <li><a class="dropdown-item" href="luxury.html">Luxury</a></li>
+            <li><a class="dropdown-item" href="Grand.html">Grand</a></li>
+          </ul>
+        </li>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="comparision.html">Comparison </a>
+              </li>
+             
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="Contact us .html">Contact Us</a>
+              </li>
+             
+            </ul>
+          </div>
+        </div>
+      </nav>
+    
+
+
+
+
+
+
+
+
+`;
